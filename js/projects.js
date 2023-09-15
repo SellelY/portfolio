7aec29a8d593f01e40f68c54e2215851605470d8
Pernilla Nadie <pernilla.nadie@outlook.com>
2023-09-15 11:09:39 +0200

    beginning of async f

--- js/projects.js
+++ js/projects.js
@@ -2,8 +2,18 @@
 
 let main = document.querySelector("main");
 
-function projects () {
-    main.innerHTML = ``;
+async function projects () {
+
+    main.innerHTML = `
+        <div id="project-container"></div>
+    `;
+
+    let projectContainer = main.querySelector("#project-container");
+
+    try {
+        let response = await fetch("")
+    }
+
 }
 
 projects();
