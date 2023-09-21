5990db01b063bc347f6b954fd97a5cb9b8cc10de
Pernilla Nadie <pernilla.nadie@outlook.com>
2023-09-21 20:37:51 +0200

    create element for projects

--- js/projects.js
+++ js/projects.js
@@ -7,7 +7,9 @@ async function projects () {
     main.innerHTML = `
         <div id="project-container">
             <p id=message></p>
-            <div id=projects></div>
+            <div id=projects>
+            <div id=loading>Loading, kindly wait...</div>
+            </div>
         </div>
     `;
 
@@ -17,17 +19,26 @@ async function projects () {
     try {
         let response = await fetch("/php/index.php");
         let data = await response.json();
+        projects.innerHTML = "";
+        console.log(data);
 
         if (!response.ok) {
             message.innerHTML = `Oops! Something went wrong, we got this from the server <span>${data.message}</span>.`;
         } else {
-            projects.innerHTML = `
-                <div id=image>
+            data.forEach( async(project) => {
+                console.log(project);
+            })
+
+            /*projects.innerHTML = `
+                <div id=project-image>
                     <image src="${data.projects[0].image}">
                     <p class=image-name>${data.projects[0].name}</p>
                 </div>
-            `;
+            `; */
         }
+
+        projects.appendChild(projectImage)
+
     } catch (error) {
         console.error(error);
     }
