4653dd211a2195505bba1b0cc8d62270c4e8adcc
Pernilla Nadie <pernilla.nadie@outlook.com>
2023-09-26 11:57:27 +0200

    all json file in project.js in

--- js/projects.js
+++ js/projects.js
@@ -2,58 +2,47 @@
 
 let main = document.querySelector("main");
 
-async function projects () {
-
-    main.innerHTML = `
-        <div id="project-container">
-            <p id=message></p>
-            <div id=projects>
-            <div id=loading>Loading, kindly wait...</div>
-            </div>
+async function projects() {
+  main.innerHTML = `
+    <div id="project-container">
+        <p id="message"></p>
+        <div id="projects">
+            <div id="loading">Loading, kindly wait...</div>
         </div>
-    `;
+    </div>
+  `;
 
-    let projectContainer = main.querySelector("#projects");
-    let message = main.querySelector("#message");
+  let projectsContainer = main.querySelector("#projects");
+  let message = main.querySelector("#message");
 
-    try {
-        let response = await fetch("/php/index.php");
-        let data = await response.json();
-        projectContainer.innerHTML = "";
-        console.log(data);
+  try {
+    let response = await fetch("/php/index.php");
+    let data = await response.json();
+    projectsContainer.innerHTML = "";
+    console.log(data);
 
-        if (!response.ok) {
-            message.innerHTML = `Oops! Something went wrong, we got this from the server <span>${data.message}</span>.`;
-        } else {
-            data.projects.forEach((project) => {
-                let projectImage = document.createElement("div");
-                projectImage.id = "project-image";
-
-                project.images.forEach((imageURL) => {
-                    let image = document.createElement("img");
-                    image.src = imageURL;
-
-                    let imageName = document.createElement("p");
-                    imageName.className = "image-name";
-                    imageName.textContent = project.name;
-
-                    let imageContainer = document.createElement("div");
-                    imageContainer.classList = "image.container";
-                    imageContainer.appendChild(image);
-                    imageContainer.appendChild(imageName);
-
-                    projectImage.appendChild(imageContainer);
-                });
-
-                projectContainer.appendChild(projectImage)
-
-            });
-        }
-
-    } catch (error) {
-        console.error(error);
+    if (!response.ok) {
+      message.innerHTML = `Oops! Something went wrong, we got this from the server <span>${data.message}</span>.`;
+    } else {
+        data.projects.forEach((project) => {
+            let image = document.createElement("img");
+            image.src = project.images[0].url;
+    
+            let imageName = document.createElement("p");
+            imageName.className = "image-name";
+            imageName.textContent = project.name;
+    
+            let imageContainer = document.createElement("div");
+            imageContainer.classList = "image-container";
+            imageContainer.appendChild(image);
+            imageContainer.appendChild(imageName);
+    
+            projectsContainer.appendChild(imageContainer);
+      });
     }
-
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-
+projects();
