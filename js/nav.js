669ea7a23059e326db388f5c1c31c7ad3de6f65e
Pernilla Nadie <pernilla.nadie@outlook.com>
2023-10-04 11:09:35 +0200

    change to nav design

--- js/nav.js
+++ js/nav.js
@@ -3,12 +3,16 @@
 let nav = document.querySelector("nav");
 
 nav.innerHTML = `
-<div class="nav-buttons">
-    <button id="hem">HEM</button>
-    <button id="projekt">PROJEKT</button>
-    <button id="sociala medier">SOCIALA MEDIER</button>
-    <button id="om-mig">OM MIG</button>
-</div>
+
+    <div class="nav-buttons">
+        <button id="om-mig"><img id="user-icon" src="/media/users.png" alt="user icon">OM MIG</button>
+    </div>
+
+    <div class="nav-buttons">
+        <button id="hem">HEM</button>
+        <button id="projekt">PROJEKT</button>
+        <button id="sociala medier">SOCIALA MEDIER</button>
+    </div>
 `;
 
 let projectBtn = nav.querySelector("#projekt");
